caafaab313fda23a94d703596fc1d74316c44da6
Aditya Kumawat <adityakumawat502@gmail.com>
2023-06-08 08:16:57 +0530

    Add files via upload

---
+++ src/common.h
@@ -0,0 +1,15 @@
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
+
+#define DEBUG
+#ifdef DEBUG
+
+#include "stdio.h"
+
+#endif
